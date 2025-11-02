578e4530d4f4cd9c36ee14b5c056e664d8d6eee4
adrian <devxsolutionsit@gmail.com>
2025-11-02 03:52:52 -0500

    11actualizacion commit del backend

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -14,7 +14,7 @@ export const verificarToken = async (req, res, next) => {
     const token = req.headers["authorization"]?.split(" ")[1];
     if (!token) return res.status(403).json({ message: "Token no proporcionado" });
 
-    const decoded = jwt.verify(token, process.env.SECRET_KEY,(err, decoded) => {
+    const decoded = jwt.verify(token, SECRET_KEY,(err, decoded) => {
     if (err) return res.status(403).json({ message: "Token inválido" });
     req.user = decoded;
     next();
