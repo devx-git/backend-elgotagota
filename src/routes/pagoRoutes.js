4bffb4645f18a009066693d84ab967082b37df2d
adrian <devxsolutionsit@gmail.com>
2025-11-03 01:31:49 -0500

    pago4actualizacion commit del backend

--- src/routes/pagoRoutes.js
+++ src/routes/pagoRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { registrarPago } from "../controllers/pagoController.js";
-import verificarToken from "../middleware/authMiddleware.js";
+import { verificarToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
