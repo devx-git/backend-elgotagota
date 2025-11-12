eece9cabd3292df6c98921b103d6ed9196d18ac9
adrian <devxsolutionsit@gmail.com>
2025-11-12 15:19:18 -0500

    endpointgoteo4 commit del backend

--- src/routes/historialRoutes.js
+++ src/routes/historialRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { verificarToken } from "../middleware/authMiddleware.js";
-import { obtenerHistorialConGoteo } from "../controllers/historialController.js";
+import { obtenerHistorialConGoteo,
+  retirarPago,
+  invertirPago,
+  reinvertirPago } from "../controllers/historialController.js";
 
 const router = express.Router();
 
